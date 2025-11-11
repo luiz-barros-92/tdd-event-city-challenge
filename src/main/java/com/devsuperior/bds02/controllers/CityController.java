95e7b79f47bd5b125803f3ba5fb532e53c7f1df8
Luiz Barros <lck.barros@gmail.com>
2025-11-10 21:00:19 -0300

    feat: findAll sorted by name in GET /cities endpoint

---
+++ src/main/java/com/devsuperior/bds02/controllers/CityController.java
@@ -0,0 +1,28 @@
+package com.devsuperior.bds02.controllers;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.services.CityService;
+
+@RestController
+@RequestMapping(value = "/cities")
+public class CityController {
+	
+	private final CityService service;
+
+	public CityController(CityService service) {
+		this.service = service;
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<CityDTO>> findAll() {
+		List<CityDTO> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+}
